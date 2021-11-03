14f0e959c4eb3d1004b7d2854eaead3e96320087
Isaias Roberto <isaias.reng@gmail.com>
2021-11-03 15:12:33 -0300

    adicionando projeto kanban

---
+++ kanban/src/componentes/Navbar/Navbar.js
@@ -0,0 +1,12 @@
+import "./navbar.css"
+import React from 'react'
+
+function Navbar(){
+    return(
+        <nav className="navbar">
+            <span>React Kanban</span>
+        </nav>
+        )
+}
+
+export default Navbar
